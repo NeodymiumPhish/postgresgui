baadbca6c6dac345cfcdf6100ccb6f69d6122366
fghazi <fikrigha@gmail.com>
2025-12-21 00:19:24 -0500

    Update alignment

--- PostgresGUI/Views/Sidebar/SavedQueryRowView.swift
+++ PostgresGUI/Views/Sidebar/SavedQueryRowView.swift
@@ -15,17 +15,19 @@ struct SavedQueryRowView: View {
     @State private var isButtonHovered = false
 
     var body: some View {
-        HStack(alignment: .top, spacing: 8) {
-            Image(systemName: "doc.text")
-                .foregroundColor(.secondary)
-                .padding(.top, 2)
-            VStack(alignment: .leading, spacing: 2) {
-                Text(query.name)
-                    .lineLimit(1)
-                Text(query.queryText.prefix(50) + (query.queryText.count > 50 ? "..." : ""))
-                    .font(.caption)
+        HStack(alignment: .center, spacing: 8) {
+            HStack(alignment: .top, spacing: 8) {
+                Image(systemName: "doc.text")
                     .foregroundColor(.secondary)
-                    .lineLimit(1)
+                    .padding(.top, 2)
+                VStack(alignment: .leading, spacing: 2) {
+                    Text(query.name)
+                        .lineLimit(1)
+                    Text(query.queryText.prefix(50) + (query.queryText.count > 50 ? "..." : ""))
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                        .lineLimit(1)
+                }
             }
             Spacer()
             if isHovered {
